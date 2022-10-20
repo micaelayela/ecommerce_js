41f9e3e390e23f3bc9ac71c2b54ccde1efa51429
micaela yelanguezian <micaelayela@gmail.com>
2022-10-20 19:02:28 -0300

    css banner

--- shoponline.js
+++ shoponline.js
@@ -87,6 +87,7 @@ const agregarAlCarrito = (prodId) => {
     }
     actualizarCarrito()
 }
+
 const eliminarDelCarrito = (prodId) => {
     const item = carrito.find((prod) => prod.id === prodId)
     const indice = carrito.indexOf(item)
